eac0a8c3f5a2bd7bb572d3e78ca0a94eb6d0e15e
Gokulnath M Prabhu <gokz1119@gmail.com>
2023-05-08 19:44:16 +0530

    Migrate project from cra to vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+// import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Home from './components/molecules/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
